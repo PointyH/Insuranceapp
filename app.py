63a8755466e775b7fd8695e03aaed3cba32a1fa2
PointyH <hal@pointon.net>
2025-10-30 10:29:03 +0000

    Update app.py to exclude sex

--- app.py
+++ app.py
@@ -11,13 +11,12 @@ with open('text_processor.pkl', 'rb') as file:
 st.title('Insurance premium predictor')
 
 age = st.slider('Age (yrs)', 18, 64)
-sex = st.selectbox('Sex', ['Male', 'Female']).lower()
 bmi = st.slider('BMI', 16, 53)
 children = st.slider('Number of children', 0, 5)
 smoker = st.selectbox('Smoker?', ['Yes', 'No']).lower()
 
-sex_num, smoker_num = text_processor.transform([[sex,smoker]])[0]
-input_data = np.array([age,sex_num,bmi,children,smoker_num]).reshape(1,-1)
+smoker_num = text_processor.transform([[smoker]])[0]
+input_data = np.array([age,bmi,children,smoker_num]).reshape(1,-1)
 
 prediction = model.predict(input_data)
 
